568a9768a931170909e4a87f896aa237b37e1cc9
RomanMereneanu <asasin84@mail.ru>
2019-05-05 15:35:21 +0300

    Add semantic-ui

--- src/js/page/App.js
+++ src/js/page/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import { Header, TextArea } from 'semantic-ui-react';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello world!!!</h1>
-    </div>
-  );
+
+class App extends Comment{
+    render(){
+        return (
+            <div>
+                <div className="headerSandbox">
+                    <Header size='hugo'>
+                        SANDBOX
+                    </Header>
+                </div>
+                <div className="editor">
+                    <TextArea rows={35} placeholder='Tell us more' />
+                </div>
+            </div>
+        );
+    }
+
 }
 
 export default App;
