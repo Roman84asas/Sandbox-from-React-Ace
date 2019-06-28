d904276de41806690bf60b943975fd95021769aa
RomanMereneanu <asasin84@mail.ru>
2019-06-28 16:36:11 +0300

    add ace-editor and value

--- src/js/page/App.js
+++ src/js/page/App.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { Header, TextArea } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
+import brace from 'brace';
+import AceEditor from 'react-ace';
+
+import 'brace/mode/javascript';
+import 'brace/theme/monokai';
 
 
-class App extends Comment{
+class App extends React.Component{
     render(){
         return (
             <div>
                 <div className="headerSandbox">
                     <Header size='hugo'>
-                        SANDBOX (Возвращаюсь в проекты)))
+                        SANDBOX
                     </Header>
                 </div>
                 <div className="editor">
-                    <TextArea rows={35} placeholder='Tell us more' />
+                    <AceEditor
+                        mode="javascript"
+                        theme="monokai"
+                        name="sandbox"
+                        showGutter={true}
+                        highlightActiveLine={true}
+                        height='500px'
+                        fontSize={16}
+                        editorProps={{
+                            $blockScrolling: true
+
+                        }}
+                    />,
                 </div>
             </div>
         );
